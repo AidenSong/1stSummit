447f00ec7074af2f411f5cb6f4842055964a4238
kiset200 <kiset200@jcobinfo.com>
2024-09-26 22:54:39 +0900

    PointService & PointServiceImpl 생성 및 작성

---
+++ src/main/java/io/hhplus/tdd/point/PointService.java
@@ -0,0 +1,15 @@
+package io.hhplus.tdd.point;
+
+
+import java.util.List;
+
+public interface PointService {
+
+    UserPoint point(long id);
+
+    List<PointHistory> history(long id);
+
+    UserPoint charge(long id, long amount) throws  Exception;
+
+    UserPoint use(long id, long amount) throws Exception;
+}
